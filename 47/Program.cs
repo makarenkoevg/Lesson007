73718e5c771cc17d4a21fdecc2fc78f9298ba4d2
makarenkoevg <makarenkoevg@mail.ru>
2022-12-13 01:59:39 +0500

    47

--- 47/Program.cs
+++ 47/Program.cs
@@ -1,1 +1,43 @@
 ﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите количество строк массива: ");
+int rows = int.Parse(ReadLine());
+
+Write("Введите количество столбцов массива: ");
+int columns = int.Parse(ReadLine());
+
+
+double [,] GetArray(int m, int n, int min, int max)
+{
+    double [,] result = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().NextDouble()*(max-min)+min;
+          
+        }
+
+    }
+    return result;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Write($"{array[i, j]:f3} ");
+        }
+        WriteLine();
+    }
+}
+
+PrintArray(GetArray(rows,columns,-10,10));
